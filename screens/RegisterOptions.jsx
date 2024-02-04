ca3c802b7c30388ed4161a93050df96d1c7f6bad
Kositthai <vkositthai@outlook.com>
2024-02-05 00:40:52 +0100

    Done create register option page UI

--- screens/RegisterOptions.jsx
+++ screens/RegisterOptions.jsx
@@ -1,57 +1,73 @@
 import React from 'react';
-import { View, Text, Button, Image, StyleSheet } from 'react-native';
-import womanIcon from '../assets/images/phone.png';
+import { View, Text, Button, StyleSheet, ImageBackground } from 'react-native';
 import { buttons } from '../src/reusable/styles/Button';
+import walkalone from '../assets/images/wave-red-black.png';
+import { layout } from '../src/reusable/styles/LayOut';
+import { TouchableOpacity } from 'react-native-web';
 
 function RegisterOptions() {
   return (
     <View style={styles.container}>
-      <View style={styles.centerBox}>
-        <Text style={styles.shortMessage}>Do you know?</Text>
-        <Text style={styles.shortMessage}>
-          "Half of the female population do not feel safe walking alone at night
-          even in busy places"
-        </Text>
-      </View>
-      <Image
-        source={womanIcon}
-        style={{ width: '50%', height: '50%', color: 'white' }}
-      />
-      <View style={styles.buttonContainer}>
-        <Button
-          title="I would like to volunteer"
-          color="red"
-          style={[buttons.btnRaduis10]}
-        />
-        <Button
-          title="I want somebody to talk to be on the way home"
-          color="red"
-          width={'50%'}
-        />
-      </View>
+      <ImageBackground source={walkalone} style={layout.fullWidthCenter}>
+        <View style={styles.centerBox}>
+          <Text style={styles.shortMessage}>Do you know?</Text>
+          <Text style={styles.shortMessage}>
+            Half of the female population<br></br>do not feel safe walking alone
+            <br></br>
+            at night even in busy places
+          </Text>
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity>
+            <View style={styles.button}>
+              <Text style={styles.whiteText}>I would like to volunteer</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <View style={styles.button}>
+              <Text style={styles.whiteText}>
+                I want somebody to talk to be on the way home
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#e6e3e3',
     flex: 1,
   },
   shortMessage: {
     fontSize: 20,
-    fontFamily: 'Frank Ruhl Libre',
-    color: 'red',
-    fontWeight: '800',
     textAlign: 'center',
+    color: 'white',
+    fontWeight: '800',
+    marginLeft: 5,
+    marginRight: 5,
+    marginBottom: 10,
+    fontFamily: 'Klee One',
   },
   centerBox: {
     display: 'flex',
-    justifyContent: 'center',
     alignItems: 'center',
+    marginTop: '30%',
   },
   buttonContainer: {
-    width: '50%',
+    marginTop: 'auto',
+    marginBottom: 30,
+  },
+  button: {
+    backgroundColor: 'rgba(248, 124, 124, 0.5)',
+    padding: 15,
+    borderRadius: 10,
+    margin: 5,
+  },
+  whiteText: {
+    color: 'white',
   },
 });
 
